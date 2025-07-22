8b3751ce137fe88bc67f5ca6abcb3fbab77cd020
Alex Zakrevsky <zakrevskyalex1323@gmail.com>
2025-07-22 16:43:55 +0300

    test: test question type validation with zod schemas

--- test/services/QuizService.test.tsx
+++ test/services/QuizService.test.tsx
@@ -1,3 +1,4 @@
+import { QuestionSchema, QuizSchema } from '../../src/models/shemas';
 import { getQuiz, getQuizzes } from '../../src/services/quizService';
 
 const quizzes = getQuizzes();
@@ -6,9 +7,22 @@ const firstQuizId = 'quiz1';
 const randomID = '13bip3f';
 
 test('get quiz by id', () => {
-  expect(getQuiz(firstQuizId)).toBe(quizzes.get(firstQuizId));
+  const quiz = getQuiz(firstQuizId);
+  expect(quiz).toBe(quizzes.get(firstQuizId));
+  expect(() => QuizSchema.parse(quiz)).not.toThrow();
 });
 
 test('quiz not found', () => {
   expect(() => getQuiz(randomID)).toThrow('Quiz not found!');
+});
+
+test('invalid question type throws', () => {
+  const invalid_type = {
+    id: '2',
+    text: 'test text2',
+    options: ['2', '1'],
+    type: 'invalid_question_type',
+    answerIndex: 1,
+  };
+  expect(() => QuestionSchema.parse(invalid_type)).toThrow();
 });
